26fe8c80cb502751a042506a8cf71fb27081a00f
jolynnk <jolynnkhoosx@gmail.com>
2023-08-17 12:53:03 +0800

    Update ShoppingCart.jsx

--- React/src/components/CheckoutPages/ShoppingCart.jsx
+++ React/src/components/CheckoutPages/ShoppingCart.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Checkout from './Checkout';
+import React from "react";
+import Checkout from "./Checkout";
 
 //to display items that have been added to shopping cart
 //allow updating/deleting of cart item quantities
 //display cost per item and total order cost
 
 const ShoppingCart = () => {
-    return (
-        <>
-            <Checkout></Checkout>
-        </>
-    );
+  return (
+    <>
+      <Checkout></Checkout>
+    </>
+  );
 };
 
 export default ShoppingCart;
