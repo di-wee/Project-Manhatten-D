97665c6692d8f7909dc67273af1cf6f670b8521b
Dionis <Dionis.w96@gmail.com>
2023-08-17 15:02:23 +0800

    category controllers done

---
+++ Backend/src/controllers/category.js
@@ -0,0 +1,9 @@
+const Category = require('../models/Category');
+
+const getAllCategories = async (req, res) => {};
+
+const getCategory = async (req, res) => {};
+
+const createCategory = async (req, res) => {};
+
+const updateCategory = async (req, res) => {};
--- Backend/src/routers/category.js
+++ Backend/src/routers/category.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router
+	.get('/category', getAllCategories)
+	.get('/category/:id', getCategory)
+	.post('/category', createCategory)
+	.put('/category/:id', updateCategory)
+	.delete('/category/:id', deleteCategory);
+
 module.exports = router;
