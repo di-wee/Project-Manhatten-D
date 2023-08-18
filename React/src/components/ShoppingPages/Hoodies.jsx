1cb63d5a1aa905139159c8bf9d1b6cd2b148f84f
Dionis <Dionis.w96@gmail.com>
2023-08-18 09:13:22 +0800

    Update Hoodies.jsx

--- React/src/components/ShoppingPages/Hoodies.jsx
+++ React/src/components/ShoppingPages/Hoodies.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import HoodiesItems from "./HoodiesItems";
+import React from 'react';
+import HoodiesItems from './HoodiesItems';
 
 //get and map/prop hoodies into HoodiesItems below
 
 const Hoodies = () => {
-  return (
-    <>
-      <Hoodies></Hoodies>
-    </>
-  );
+	return (
+		<>
+			<HoodiesItems></HoodiesItems>
+		</>
+	);
 };
 
 export default Hoodies;
