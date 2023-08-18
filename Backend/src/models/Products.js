b38575084de1c507a044f7fda4a015867c6142f7
Dionis <Dionis.w96@gmail.com>
2023-08-18 10:35:34 +0800

    latest edits on product models

--- Backend/src/models/Products.js
+++ Backend/src/models/Products.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: String, required: true },
-  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
-  subcategory: { type: Schema.Types.ObjectId, ref: 'Subcategory' },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+	name: { type: String, required: true },
+	description: { type: String, required: true },
+	price: { type: Number, required: true },
+	image: { type: String, required: true },
+	category: { type: String, enum: [`Men`, `Women`], required: true },
+	subcategory: {
+		type: String,
+		enum: ['Bottoms', 'Shoes', 'Bags', 'Tops', 'Accessories'],
+		required: true,
+	},
 });
 
 const Product = mongoose.model('Product', productSchema);
